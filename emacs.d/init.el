d176f6dddd4f74b4b1fe8c397c07ae8c6d89b7be
Zak Kristjanson <ubercow1337@gmail.com>
2015-03-07 04:37:45 -0800

    [Emacs] Remove CL dependency and change some UI
    I like the menu items on mac. Might help me learn emacs. Emacs also told
    me that I should feel bad for using CL, so I rewrote it.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,4 @@
-
 ;; setup gui
-(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (set-frame-font "Droid-Sans-Mono-15" t)
@@ -9,7 +7,6 @@
 
 ;; setup packages
 (require 'package)
-(require 'cl)
 (add-to-list 'package-archives
              '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives
@@ -48,9 +45,12 @@
     ))
 
 (defun udots-packages-installed-p ()
-  (loop for p in udots-packages
-	when (not (package-installed-p p)) do (return nil)
-	finally (return t)))
+  "check if all packages are installed"
+  (catch 'udots-package-installed
+    (dolist (p udots-packages)
+      (when (not (package-installed-p p))
+        (throw 'udots-package-installed nil)))
+    t))
 
 (defun udots-list-foreign-packages ()
   "lists packages not part of udots"
