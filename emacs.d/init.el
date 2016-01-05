2d4b7a60152d56fc3234ca0856e8be9de6c8ca06
Zak Kristjanson <ubercow1337@gmail.com>
2016-01-05 01:47:00 -0800

    new emacs.
    better packages i actually like and understand including use of the
    use-package package. new theme. no evil!

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,9 +1,6 @@
 ;; setup gui
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(set-frame-font "Droid-Sans-Mono-15" t)
-(add-to-list 'default-frame-alist
-             '(font . "Droid-Sans-Mono-15"))
 
 ;; setup packages
 (require 'package)
@@ -15,284 +12,170 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
+;; setup use-package, which is used later to actually install packages
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-(defvar udots-packages
-  '(
-    exec-path-from-shell
-    moe-theme
-    company
-    guide-key
-    diminish
-    smartparens
-    rainbow-delimiters
-    magit
-    helm
-    whitespace
-    dired+
-    evil
-    evil-leader
-    evil-nerd-commenter
-    markdown-mode
-    go-mode
-    company-go
-    flycheck
-    editorconfig
-    org
-    git-gutter
-    js2-mode
-    json-mode
-    projectile
-    helm-projectile
-    helm-ag
-    web-mode
-    powerline
-    csharp-mode
-    omnisharp
-    floobits
-    zenburn-theme
-    clojure-mode
-    cider
-    restclient
-    smartparens
-    evil-lisp-state
-    evil-smartparens
-    ))
-
-(defun udots-packages-installed-p ()
-  "check if all packages are installed"
-  (catch 'udots-package-installed
-    (dolist (p udots-packages)
-      (when (not (package-installed-p p))
-        (throw 'udots-package-installed nil)))
-    t))
-
-(defun udots-list-foreign-packages ()
-  "lists packages not part of udots"
-  (interactive)
-  (package-show-package-list
-   (set-difference package-activated-list udots-packages)))
-
-(defun udots-install-packages ()
-  "installs udots packages"
-  (interactive)
-  (unless (udots-packages-installed-p)
-    (message "%s" "Now refreshing package database...")
-    (package-refresh-contents)
-    (message "%s" "Done")
-    (dolist (p udots-packages)
-      (when (not (package-installed-p p))
-	(package-install p)))))
-
-(udots-install-packages)
-
-;; PATH
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-(exec-path-from-shell-copy-env "GOPATH")
-
-;; powerline
-(require 'powerline)
-
-;; theme
-(require 'moe-theme)
-(moe-light)
-(powerline-moe-theme)
+(setq use-package-verbose t)
+(require 'use-package)
 
 ;; backups
-(defvar udots-backup-directory (concat user-emacs-directory "backups"))
-(if (not (file-exists-p udots-backup-directory))
-    (make-directory udots-backup-directory t))
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "auto-save-list") t)))
 
-(setq backup-directory-alist
-      `((".*" . ,udots-backup-directory)))
-;(setq auto-save-file-name-transforms
-      ;`((".*" ,udots-backup-directory t)))
-(setq make-backup-files t
-      backup-by-copying t
-      version-control t
-      delete-old-versions t
-      kept-old-versions 6
-      kept-new-versions 9
-      auto-save-default t
-      auto-save-timeout 20
-      auto-save-interval 200)
+;; history
+(setq savehist-file (concat user-emacs-directory "savehist"))
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+	search-ring
+	regexp-search-ring))
 
-;; general hackery
-(setq initial-major-mode 'text-mode)
-(setq-default show-trailing-whitespace t)
-(show-paren-mode t)
-(setq-default indent-tabs-mode nil)
-(setq tab-width 2)
-(blink-cursor-mode 0)
+;; make sentences end with single space.
+;; Makes M-e (forward-sentence) and M-a (backward-sentence) work like a sane human
+(setq sentence-end-double-space nil)
+
+;; allows y or n in place of yes or no
 (fset 'yes-or-no-p 'y-or-n-p)
-(electric-indent-mode)
+
+;; default to 120 char fill
+(setq-default fill-column 120)
+
+;; highlights matching parens
+(show-paren-mode t)
+
+;; stop the damn cursor blinking
+(blink-cursor-mode 0)
+
+;; lines and columns
 (setq line-number-mode t)
 (setq column-number-mode t)
 
-;; make <escape> quit as much as possible
-;; s/o to spacemacs for this tip
-(define-key minibuffer-local-map (kbd "<escape>") 'keyboard-escape-quit)
-;; (define-key evil-visual-state-map (kbd "<escape>") 'keyboard-quit)
-(define-key minibuffer-local-ns-map (kbd "<escape>") 'keyboard-escape-quit)
-(define-key minibuffer-local-completion-map (kbd "<escape>") 'keyboard-escape-quit)
-(define-key minibuffer-local-must-match-map (kbd "<escape>") 'keyboard-escape-quit)
-(define-key minibuffer-local-isearch-map (kbd "<escape>") 'keyboard-escape-quit)
+;; indent on newline
+(electric-indent-mode)
 
-;; custom emacs key bindings
+;; set default major mode to text-mode
+(setq initial-major-mode 'text-mode)
 
-;; emacs lisp
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (setq flycheck-disabled-checkers '(emacs-lisp-checkdoc))
-            (setq mode-name "Elisp")
-            (rainbow-delimiters-mode t)))
-
-;; clojure
-(add-hook 'clojure-mode-hook
-          (lambda()
-            (rainbow-delimiters-mode t)))
-
-;; company
-(setq company-idle-delay 0.5)
-(setq company-tooltip-limit 10)
-(setq company-minimum-prefix-length 2)
-(setq company-tooltip-flip-when-above t)
-
-(global-company-mode 1)
-(require 'company-go)
-
-;; git gutter
-(global-git-gutter-mode t)
-
-;; smartparens
-(require 'smartparens-config)
-(add-hook 'smartparens-enabled-hook #'evil-smartparens-mode)
-
-;;dired+
-(require 'dired+)
-
-;; flycheck
-(add-hook 'after-init-hook
-          #'global-flycheck-mode)
-
-;; guide key
-(setq guide-key/guide-key-sequence t)
-(guide-key-mode 1)
-
-;; ido mode
+;; ido mode, for now
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 
-;; helm
-(require 'helm-config)
-(when (executable-find "curl")
-  (setq helm-google-suggest-use-curl-p t))
+;; packages and package-specific
+;; (use-package zenburn-theme :ensure t)
+;; load all of these cause I might be in the "mood" for a theme
+(use-package solarized-theme :ensure t)
+(use-package moe-theme :ensure t)
+(use-package spacemacs-theme :ensure t)
 
-(setq helm-split-window-in-side-p t
-      helm-move-to-line-cycle-in-source t
-      helm-ff-search-library-in-sexp t
-      helm-scroll-amount 8
-      helm-ff-file-name-history t)
+(defadvice load-theme (before theme-dont-propagate activate)
+  (mapcar #'disable-theme custom-enabled-themes))
 
-(helm-mode t)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; (load-theme 'moe-light t)
+;; (load-theme 'moe-dark t)
+(load-theme 'spacemacs-dark t)
+;; (load-theme 'solarized-dark t)
 
-;; projectile
-(require 'helm-projectile)
-(projectile-global-mode)
+;; smex = ido for M-x
+(use-package smex
+  :ensure t
+  :init
+  (smex-initialize)
+  :bind
+  (("M-x" . smex)
+   ("M-X" . smex-major-mode-commands)))
 
-;; evil
-(evil-mode t)
-(define-key evil-motion-state-map (kbd "C-u") 'evil-scroll-up)
-(evil-ex-define-cmd "b[uffer]" 'helm-buffers-list)
-(evil-ex-define-cmd "e[dit]" 'find-file)
+;; x = delete
+;; m = swap window
+;; v = split vertically
+;; b = split horizontally
+;; n = select previous
+;; i = maximize window
+;; o = maximize current window
+(use-package ace-window
+  :ensure t
+  :config
+  (setq aw-dispatch-always t)
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  :bind
+  (("M-p" . ace-window)))
 
-(define-key evil-ex-map "b" 'helm-buffers-list)
-(define-key evil-ex-map "e" 'find-file)
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0.5)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 2)
+  (setq company-tooltip-flip-when-above t)
+  (global-company-mode))
 
-;; evil-leader keybindings
-(setq evil-leader/in-all-states t
-      evil-leader/leader "SPC"
-      evil-leader/non-normal-prefix "s-")
+(use-package company-quickhelp
+  :ensure t)
 
-(global-evil-leader-mode)
+(use-package smartparens
+  :ensure t
+  :config
+  (require 'smartparens-config)
+  (sp-local-pair 'emacs-lisp-mode "'" nil :actions :rem)
+  (add-hook 'clojure-mode-hook (lambda () (smartparens-strict-mode t))))
 
-(evil-leader/set-key
-  "u" 'universal-argument
-  "!" 'shell-command
-  "b" 'helm-buffers-list
-  ":" 'eval-expression
-  "x" 'execute-extended-command)
+(use-package which-key
+  :ensure t
+  :diminish which-key-mode
+  :config
+  (which-key-mode 1)
+  (setq which-key-idle-delay 0.5))
 
-(evil-leader/set-key
-  "a u" 'undo-tree-visualize)
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
+  (exec-path-from-shell-copy-env "GOPATH"))
 
-(evil-leader/set-key
-  "g n" 'git-gutter:next-hunk
-  "g p" 'git-gutter:previous-hunk
-  "g h s" 'git-gutter:stage-hunk
-  "g h r" 'git-gutter:revert-hunk
-  "g s" 'magit-status)
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook (lambda () (rainbow-delimiters-mode t)))
+  (add-hook 'clojure-mode-hook (lambda () (rainbow-delimiters-mode t))))
 
-(evil-leader/set-key
-  "p p" 'helm-projectile-switch-project
-  "p d" 'projectile-dired
-  "p f" 'helm-projectile-find-file
-  "p b" 'helm-projectile-switch-to-buffer
-  "p a" 'helm-projectile-ag)
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode))
 
-(evil-leader/set-key
-  "f m" 'make-frame
-  "f d" 'delete-frame)
+(use-package magit
+  :ensure t
+  :bind
+  (("C-c m" . magit-status)))
 
-(evil-leader/set-key-for-mode 'emacs-lisp-mode
-  "c x" 'eval-defun)
+;; M-; to comment and uncomment lines
+(use-package evil-nerd-commenter
+  :ensure t
+  :config
+  (evilnc-default-hotkeys))
 
-(evil-leader/set-key-for-mode 'go-mode
-  "c f" 'gofmt
-  "c r" 'go-remove-unused-imports
-  "c d" 'godoc-at-point
-  "c a" 'go-import-add)
+;; Org mode and friends
+(use-package org
+  :ensure t)
 
-(setq evil-lisp-state-global t)
-(require 'evil-lisp-state)
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode t))))
 
-;; smartparens
-(require 'smartparens-config)
+(use-package clojure-mode
+  :ensure t)
 
-;; go
-(add-hook 'go-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (local-set-key (kbd "C-c C-f") 'gofmt)
-                          (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-                          (local-set-key (kbd "C-c C-d") 'godoc-at-point)))
+(use-package cider
+  :ensure t
+  :config
+  (add-hook 'cider-mode-hook (lambda () (eldoc-mode t))))
 
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-;; webmode
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
-;; c#
-; (add-hook 'csharp-mode-hook 'omnisharp-mode)
-; (eval-after-load 'company
-;   '(add-to-list 'company-backends 'company-omnisharp))
-
-;; org
-(add-hook 'org-mode-hook (lambda()
-                           (set show-trailing-whitespace -1)))
-
-;; custom
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("355b84fe9816309bfbe3b26074f75daa238b8e3d1d56b69baba1739bc1005e93" "cbef37d6304f12fb789f5d80c2b75ea01465e41073c30341dc84c6c0d1eb611d" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(provide 'init)
