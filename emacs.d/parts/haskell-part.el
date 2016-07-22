86a098763194adae3169106c007e2813072393d7
Zak Kristjanson <ubercow1337@gmail.com>
2016-07-22 16:14:38 -0700

    Improve haskell support

--- emacs.d/parts/haskell-part.el
+++ emacs.d/parts/haskell-part.el
@@ -1,5 +1,14 @@
 (use-package haskell-mode
-  :ensure t)
+  :ensure t
+  :init
+  (setq haskell-process-type 'cabal-repl))
+
+(use-package flycheck-haskell
+  :ensure t
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
+
+;
 
 ;(use-package ghc-mod
 ;  :ensure t)
